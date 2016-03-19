88ccaf52750952929b242e0b33260209c5257965
Amom Mendes <amommendes@hotmail.com>
2016-03-19 02:06:49 -0300

    Update code.R

--- code.R
+++ code.R
@@ -15,6 +15,7 @@ summary (g)
 gr<-table (g)
 gr
 
+
 # Number of Edges
 nrow (g)
 #Number of Vertices
@@ -58,27 +59,31 @@ short<- function (m){
  }
 
 
-clos<- function (m, sort=T){
+clos<- function (m, sort=T, verbose=F){
     ## m = Adjacency Matrix
     ## sort = should vertices be sorted by their closeness centrality?
-    
+    ## Verbose = should print result?
     m1<-short(m)
     c<- 1/(rowSums(m1))
+    names(c)<-as.integer(names (c))-1
     
+    if (verbose==T){ 
+    print (paste('The node',names(c[c==max(c)]),
+        'has the higest closeness centrality value in the rank'))}
+
     if (sort == T) {
-        return (sort (c))
+        return (sort (c, decreasing = T))
     } else {return (c)}
-    
+
 }
 
 init.time<-proc.time()
 cc<-clos(gr)
-init.time -proc.time()
+proc.time() - init.time
 
-
-# Here we will implement the BFS algorithm to define the shortest path [in 
-# construction, meanwhile we implemented a test to assess if one node is in the
-# adjacencies of source node]
+# Here we will implement the BFS algorithm to define the shortest path (in 
+# construction, meanwhile we implemented a test to assess if one nome is in the
+# adjacencies of source node)
 
 short2<- function (m, ad, s, d){
     if (!is.list(ad))
@@ -110,9 +115,83 @@ short2(gr,ad,1,6)
 # Using iGraph library
 # this library uses C and is very fast...
 
-library(igraph)
 
+g1<-graph.adjlist(ad)
+graph.ad
 ini.t<-proc.time()
 a<-simplify(graph.adjacency(gr, mode = 'un'))
 sort(closeness(a))
+sort(closeness(g1))
 proc.time() - ini.t
+
+# Just for fun, we can visualize this graph
+# (following Katherine Ognyanova tutorial - kateto.net/network-visualization)
+
+## Creating a graph object with igrph package
+library(igraph)
+a<-simplify(graph.adjacency(gr, mode = 'un'))
+plot (a)
+
+# Some more packages
+test.pack<- function (name=NULL)
+    if (is.null(name)){
+        warning('Please, provide a string with package name')
+    } else { 
+        if  (! name %in% rownames(installed.packages())){
+            install.packages(paste(name))
+        } else {
+            
+            library(paste(name), character.only = T)}
+    }
+
+test.pack("sna")
+test.pack("ndtv")
+test.pack('network')
+
+## Plotting iGraph
+
+plot(a,
+     vertex.color=rgb(0, 0, .3, alpha=.8),
+     vertex.frame.color='black',
+     vertex.size=10,
+     vertex.label.cex=.4,
+     vertex.label.font=2,
+     margin=c(0,0,0,0),
+     main='Prospective customers social network',
+     edge.curved=.3,
+     layout= layout.kamada.kawai,
+     mark.groups=45,mark.col="#C5E5E7",mark.border='black'
+)
+tkid <- tkplot(a) #tkid is the id of the tkplot that will open
+l <- tkplot.getcoords(tkid) # grab the coordinates from tkplot
+plot(a, layout=l)
+
+
+detach(package:igraph)
+
+library(network)
+
+c1<-clos(gr, sort = F) #closeness from clos function
+
+net3 <- network(g, matrix.type="edgelist", loops=F, 
+                directed = F,multiple=F, ignore.eval = F)
+
+plot(net3, 
+     vertex.cex= scale (c1),
+     vertex.col="tomato"
+     )
+
+test.pack("networkD3")
+
+g2<-data.frame (source=g$V1-1, target=g$V2-1)
+
+n<-data.frame(name=as.factor(0:99),group=ifelse (c1 >= max(c1), "Influencer","Followers")
+           , size=c1*2)
+
+forceNetwork(Links = g2, Nodes = n, Source="source", Target="target",
+             NodeID = "name",  Group = "group",linkColour = "#afafaf", 
+             fontSize=10, zoom=T, legend=T, fontFamily = 'Calibri',
+             colourScale = JS("d3.scale.category10()"),
+             Nodesize='size', opacity = 0.8, charge=-500, bounded = F,
+             width = 800, height = 600)    
+
